66fc72471107ee1b8fa54497ccd1118e1f00b32e
Evgeny-Git79 <evgeny.zabolotnikov@gmail.com>
2025-03-28 11:53:21 +1000

    1

---
+++ main.py
@@ -0,0 +1,25 @@
+from sqlite3.dbapi2 import paramstyle
+import requests
+import pprint
+
+#Zadanie1
+params ={
+    'q': 'html'
+}
+response = requests.get('https://api.github.com', params= params)
+print(response.status_code)
+print(response.json())
+#Zadanie2
+params1 ={
+    'userId': '1'
+}
+response1 = requests.get('https://jsonplaceholder.typicode.com/posts', params= params1)
+print(response1.status_code)
+pprint.pprint(response1.json())
+
+#zadanie3
+params3 = {'title': 'foo', 'body': 'bar', 'userId': 1}
+response2 = requests.post('https://jsonplaceholder.typicode.com/posts', params=params3)
+print(response2.status_code)
+pprint.pprint(response2.json())
+
